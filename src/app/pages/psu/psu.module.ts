630110a97e382361efd24eacf662610693dd764a
Khairunnisa <khairunnisa2@btpn.com>
2020-03-06 22:37:39 +0700

    update beranda tab

--- src/app/pages/psu/psu.module.ts
+++ src/app/pages/psu/psu.module.ts
@@ -3,7 +3,14 @@ import {PsuRoutingModule, routedComponents} from './psu-routing.module';
 import {PsuComponent} from './psu.component';
 import { PsuKawasanPermukimanComponent } from './psu-kawasan-permukiman/psu-kawasan-permukiman.component';
 import {BerandaComponent} from './beranda/beranda.component';
-import {NbCardModule, NbTabsetModule} from "@nebular/theme";
+import {NbCardModule, NbTabsetModule} from '@nebular/theme';
+import { ChartModule } from 'angular2-chartjs';
+import {BerandaPieComponent} from './beranda/beranda-pie.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {AgmCoreModule} from '@agm/core';
+import {MapsModule} from '../maps/maps.module';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 
 @NgModule({
@@ -12,11 +19,18 @@ import {NbCardModule, NbTabsetModule} from "@nebular/theme";
     PsuComponent,
     PsuKawasanPermukimanComponent,
     BerandaComponent,
+    BerandaPieComponent,
   ],
   imports: [
     PsuRoutingModule,
     NbTabsetModule,
     NbCardModule,
+    ChartModule,
+    NgxEchartsModule,
+    NgxChartsModule,
+    AgmCoreModule,
+    MapsModule,
+    Ng2SmartTableModule,
   ],
 })
 export class PsuModule {
