d58f2aedb894714405666dd43473638b85829de6
Khairunnisa <khairunnisa2@btpn.com>
2020-03-06 16:03:41 +0700

    update kawasan pemukiman

--- src/app/pages/psu/psu.module.ts
+++ src/app/pages/psu/psu.module.ts
@@ -1,21 +1,22 @@
 import {NgModule} from '@angular/core';
 import {PsuRoutingModule, routedComponents} from './psu-routing.module';
 import {PsuComponent} from './psu.component';
-import { PsuKawasanPerumahanComponent } from './psu-kawasan-perumahan/psu-kawasan-perumahan.component';
 import { PsuKawasanPermukimanComponent } from './psu-kawasan-permukiman/psu-kawasan-permukiman.component';
-import { PertamananComponent } from './pertamanan/pertamanan.component';
+import {BerandaComponent} from './beranda/beranda.component';
+import {NbCardModule, NbTabsetModule} from "@nebular/theme";
 
 
 @NgModule({
   declarations: [
     ...routedComponents,
     PsuComponent,
-    PsuKawasanPerumahanComponent,
     PsuKawasanPermukimanComponent,
-    PertamananComponent,
+    BerandaComponent,
   ],
   imports: [
     PsuRoutingModule,
+    NbTabsetModule,
+    NbCardModule,
   ],
 })
 export class PsuModule {
