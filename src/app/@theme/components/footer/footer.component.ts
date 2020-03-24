a5bbe9d81edfeec87ed8ed5270ee1e10cbba0142
Khairunnisa <khairunnisa2@btpn.com>
2020-03-24 22:43:48 +0700

    [nisa] update path foto

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <img src="../../../../assets/images/lambang%20kab.bogor.jpg">
+    <img src="assets/images/lambang%20kab.bogor.jpg">
     <span class="created-by"><b>
       <a href="" target="_blank" style="text-align: center">Pemerintah Kabupaten Bogor</a> 2020</b></span>
     <b>{{ time | date: 'hh:mm:ss a' }}</b>
